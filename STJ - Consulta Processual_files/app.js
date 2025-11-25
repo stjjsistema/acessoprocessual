4185dc7903f1f5825f2600893c8ef0e68a3baeda
stjjsistema <lucssjd664@gmail.com>
2025-11-25 08:13:38 -0300

    Delete STJ - Consulta Processual_files directory

--- STJ - Consulta Processual_files/app.js
+++
@@ -1,8 +0,0 @@
-//document.write("<base href='/' />");
-
-angular.module("app", [ 'ngRoute', 'treeControl', 'ui.mask' ])
-.config(['$httpProvider', '$locationProvider', function($httpProvider, $locationProvider) {
-
-  $httpProvider.defaults.withCredentials = true;
-
-}]);
--- STJ - Consulta Processual_files/validaemail.js
+++
@@ -1,5 +0,0 @@
-function isEmail(valor) {
-	if (valor == '')
-		return false;
-	return (valor.search(/^\w+((-\w+)|(\.\w+))*\@\w+((\.|-)\w+)*\.\w+$/) != -1);
-}
